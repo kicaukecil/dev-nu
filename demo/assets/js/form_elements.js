6ad9e9cc153b3c263ca74323f630a0460d010a19
valadoo <aris.suryamas@valadoo.com>
2016-09-29 20:01:12 +0700

    mengubah halaman dashboard

--- demo/assets/js/form_elements.js
+++ demo/assets/js/form_elements.js
@@ -36,35 +36,11 @@
           prefetch: 'http://revox.io/json/countries-list.json'
         });
 
-          var bestPictures = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            prefetch: 'http://revox.io/json/drop-countries.json',
-            remote: {
-              url: 'http://revox.io/json/drop-countries.json',
-              wildcard: '%QUERY'
-            }
-          });
         // passing in `null` for the `options` arguments will result in the default
         // options being used
         $('.sample-typehead').typeahead(null, {
           name: 'countries',
           source: countries
-        });
-
-        //Custom Template
-        $('#custom-templates .typeahead').typeahead(null, {
-              name: 'best-pictures',
-              display: 'value',
-              source: bestPictures,
-              templates: {
-                empty: [
-                  '<div class="empty-message">',
-                    'unable to find any Best Picture winners that match the current query',
-                  '</div>'
-                ].join('\n'),
-                suggestion: Handlebars.compile('<div>{{value}}– {{year}}</div>')
-              }
         });
 
         $('#daterangepicker').daterangepicker({
